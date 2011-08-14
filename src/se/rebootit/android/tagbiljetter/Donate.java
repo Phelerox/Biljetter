faa0910fbf1f3808badfb0230d0651ec58b55cf6
Erik Fredriksen <erik@fredriksen.se>
2011-08-14 03:05:25 +0200

    Added Flattr to the donate Activity

--- src/se/rebootit/android/tagbiljetter/Donate.java
+++ src/se/rebootit/android/tagbiljetter/Donate.java
@@ -4,24 +4,35 @@ import java.util.*;
 
 import android.app.*;
 import android.content.*;
+import android.net.*;
 import android.os.*;
 import android.view.*;
-import android.view.ContextMenu.*;
 import android.view.View.*;
 import android.widget.*;
-import android.widget.AdapterView.*;
-
 /**
  * Donate is the class that shows the Flattr button for this project
  * 
  * @author Erik Fredriksen <erik@fredriksen.se>
  */
-public class Donate extends Activity
+public class Donate extends Activity implements OnClickListener
 {
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.donate);
+		
+		((Button)findViewById(R.id.btnFlattr)).setOnClickListener(this);
+	}
+	
+	public void onClick(View v)
+	{
+		switch(v.getId())
+		{
+			case R.id.btnFlattr:
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://flattr.com/thing/371293"));
+				startActivity(browserIntent);
+				break;
+		}
 	}
 }
