fdb8dbeafa56246af83c545039129f72fc901697
Erik Fredriksen <erik@fredriksen.se>
2011-08-24 14:33:41 +0200

    Added support for Upplands Lokaltrafik, Östgötatrafiken, X-trafik, Hallandstrafiken and Jönköpings länstrafik.

--- src/se/rebootit/android/tagbiljetter/models/TransportCompany_Upplandslokaltrafik.java
+++ src/se/rebootit/android/tagbiljetter/models/TransportCompany_Upplandslokaltrafik.java
@@ -10,34 +10,28 @@ import java.util.*;
 
 import android.os.*;
 
-public class TransportCompany_Varmlandstrafiken extends TransportCompany implements Parcelable
+public class TransportCompany_Upplandslokaltrafik extends TransportCompany implements Parcelable
 {
 	String[] months = new String[] { "jan", "feb", "mar", "apr", "maj", "jun", "jul", "aug", "sep", "okt", "nov", "dec" };
 	
-	public TransportCompany_Varmlandstrafiken() { };
+	public TransportCompany_Upplandslokaltrafik() { };
 	
-	public TransportCompany_Varmlandstrafiken(String name, String phonenumber) {
+	public TransportCompany_Upplandslokaltrafik(String name, String phonenumber) {
 		super(name, phonenumber);
 	}
 	
 	public long getTicketTimestamp(String message) {
 		String[] data = getMessageParts(message);
 
-		if (data[0] != null && data[1] != null && data[2] != null && data[3] != null) {
+		if (data[0] != null && data[1] != null) {
 			try {
-				int year = Integer.parseInt("20"+data[3]);
-				int month = (java.util.Arrays.asList(this.months).indexOf(data[2])+1);
-				int day = Integer.parseInt(data[1]);
-
-				String date = year+"-"+(month < 10 ? "0"+month : month)+"-"+(day < 10 ? "0"+day : day);
-				
-				return new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(year+"-"+month+"-"+day+" "+data[0]).getTime();
+				return new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(data[0]+" "+data[1]).getTime();
 			} catch (Exception e) { e.printStackTrace(); }
 		}
 		return 0;
 	}
 	
-	private TransportCompany_Varmlandstrafiken(Parcel in) {
+	private TransportCompany_Upplandslokaltrafik(Parcel in) {
 		this.id = in.readInt();
 		this.logo = in.readString();
 		this.name = in.readString();
@@ -55,13 +49,13 @@ public class TransportCompany_Varmlandstrafiken extends TransportCompany impleme
 		out.writeTypedList(this.types);
 	}
 
-	public static final Parcelable.Creator<TransportCompany_Varmlandstrafiken> CREATOR = new Parcelable.Creator<TransportCompany_Varmlandstrafiken>() {
-		public TransportCompany_Varmlandstrafiken createFromParcel(Parcel in) {
-			return new TransportCompany_Varmlandstrafiken(in);
+	public static final Parcelable.Creator<TransportCompany_Upplandslokaltrafik> CREATOR = new Parcelable.Creator<TransportCompany_Upplandslokaltrafik>() {
+		public TransportCompany_Upplandslokaltrafik createFromParcel(Parcel in) {
+			return new TransportCompany_Upplandslokaltrafik(in);
 		}
 
-		public TransportCompany_Varmlandstrafiken[] newArray(int size) {
-			return new TransportCompany_Varmlandstrafiken[size];
+		public TransportCompany_Upplandslokaltrafik[] newArray(int size) {
+			return new TransportCompany_Upplandslokaltrafik[size];
 		}
 	};
 
