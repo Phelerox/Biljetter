d2c3a3c9b00d2d271032ee013a92fa69b23e19e7
Erik Fredriksen <erik@fredriksen.se>
2011-09-03 13:10:39 +0200

    Fixed the background on the even row

--- src/se/rebootit/android/tagbiljetter/TicketListAdapter.java
+++ src/se/rebootit/android/tagbiljetter/TicketListAdapter.java
@@ -55,7 +55,7 @@ public class TicketListAdapter extends BaseAdapter
 		
 		// Give even rows a background color
 		if (position % 2 == 1) {
-			itemLayout.setBackgroundColor(0x30555555);
+			itemLayout.setBackgroundColor(0xaa555555);
 		}
 		
 		return itemLayout;
